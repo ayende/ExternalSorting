5de85d73818ced21122ca9476fa11baefd1aca45
Ayende Rahien <ayende@ayende.com>
2013-12-20 13:18:14 +0200

    Initial project Implementing basic heap

---
+++ ExternalSorting.Tests/HeapTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ExternalSorting.Tests
+{
+    public class HeapTests
+    {
+	    readonly Heap<int> _heap = new Heap<int>(10, Comparer<int>.Default);
+
+	    [Fact]
+	    public void CountStartsAtZero()
+	    {
+		    Assert.Equal(0, _heap.Count);
+	    }
+
+		[Fact]
+		public void AfterEnqueueCountIs1()
+		{
+			_heap.Enqueue(10);
+			Assert.Equal(1, _heap.Count);
+		}
+		[Fact]
+		public void AfterEnqueueMultipleItemsCountIs2()
+		{
+			_heap.Enqueue(10);
+			_heap.Enqueue(7);
+			Assert.Equal(2, _heap.Count);
+		}
+
+		[Fact]
+		public void CanDequeueInOrder()
+		{
+			_heap.Enqueue(10);
+			_heap.Enqueue(7);
+			_heap.Enqueue(17);
+
+			Assert.Equal(7, _heap.Dequeue());
+			Assert.Equal(10, _heap.Dequeue());
+			Assert.Equal(17, _heap.Dequeue());
+		}
+
+
+		[Fact]
+		public void CanDequeueInOrder2()
+		{
+			_heap.Enqueue(10);
+			_heap.Enqueue(7);
+			_heap.Enqueue(127);
+			_heap.Enqueue(2);
+			_heap.Enqueue(7);
+			_heap.Enqueue(17);
+
+			Assert.Equal(2, _heap.Dequeue());
+			Assert.Equal(7, _heap.Dequeue());
+			Assert.Equal(7, _heap.Dequeue());
+			Assert.Equal(10, _heap.Dequeue());
+			Assert.Equal(17, _heap.Dequeue());
+			Assert.Equal(127, _heap.Dequeue());
+		}
+    }
+}
---
+++ ExternalSorting/Heap.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ExternalSorting
+{
+	public class Heap<T>
+	{
+		private readonly IComparer<T> _comparer;
+		private readonly T[] _values;
+
+		public int Count { get; private set; }
+
+		public Heap(int size, IComparer<T> comparer)
+		{
+			_comparer = comparer;
+			_values = new T[size];
+		}
+
+		public void Enqueue(T value)
+		{
+			if (Count >= _values.Length)
+				throw new InvalidOperationException("Heap full");
+			var index = Count;
+			Count++;
+			_values[index] = value;
+
+			BubbleUp(index);
+		}
+
+		private void BubbleUp(int index)
+		{
+			while (index > 0)
+			{
+				var parentIndex = (index - 1) / 2;
+				if (_comparer.Compare(_values[index], _values[parentIndex]) > 0)
+					break;
+				var parent = _values[parentIndex];
+				_values[parentIndex] = _values[index];
+				_values[index] = parent;
+				index = parentIndex;
+			}
+		}
+
+		private void TrickleDown()
+		{
+			int index = 0;
+			var childIndex = (index * 2) + 1;
+			while (childIndex < Count)
+			{
+				if (childIndex + 1 < Count &&
+					_comparer.Compare(_values[childIndex], _values[childIndex + 1]) > 0)
+				{
+					childIndex++;
+				}
+				var tmp = _values[index];
+				_values[index] = _values[childIndex];
+				_values[childIndex] = tmp;
+				index = childIndex;
+				childIndex = (index * 2) + 1;
+			}
+			BubbleUp(index);
+		}
+
+		public T Dequeue()
+		{
+			if (Count == 0)
+				throw new InvalidOperationException("Heap empty");
+
+			var val = _values[0];
+			Count--;
+			_values[0] = _values[Count];
+			TrickleDown();
+			_values[Count] = default (T);
+
+			return val;
+		}
+	}
+}
